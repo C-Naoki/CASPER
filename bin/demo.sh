c94e6f7dbf4bb09eebab59d325d8b87f2789be8a
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-08-03 17:57:57 +0900

    🎨 style: add demo.sh
    - sh bin/demo.sh, without activation virtual environment (execute `poetry shell`)

---
+++ bin/demo.sh
@@ -0,0 +1,3 @@
+poetry run python casper_main.py --iter_mod 7 --run_mode 0 --linear --modeltype notears --s0 20 \
+    --d 10 --n 2000 --trial 5 --seed 0 \
+    --lambda1 0.002 --lambda2 0.002  --graph_type "ER" --batch_size 500 --linear_sem_type gauss
